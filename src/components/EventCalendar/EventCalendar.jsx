95753d07664796be021059f13bebdf185955fbfa
marten-lucas <be_smarten@yahoo.de>
2025-08-01 07:07:52 +0200

    showDaysOutsideCurrentMonth

--- src/components/EventCalendar/EventCalendar.jsx
+++ src/components/EventCalendar/EventCalendar.jsx
@@ -81,6 +81,7 @@ export default function EventCalendar({ scenarioId }) {
         loading={loading}
         onMonthChange={handleMonthChange}
         renderLoading={() => <DayCalendarSkeleton />}
+        showDaysOutsideCurrentMonth
         slots={{
           day: (props) => (
             <EventDay {...props} eventsByDay={eventsByDay} />
